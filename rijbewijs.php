9e6e9645dada204950eed5e2f46c27d713f3874b
IssamAmajoud <45203821+IssamAmajoud@users.noreply.github.com>
2020-04-04 22:12:43 +0200

    Add files via upload

---
+++ rijbewijs.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rijbewijs</title>
+</head>
+<body>
+    <form method="post">
+    	<h2> Hoe oud ben je? </h2>
+    	<input type="number" name="cijfer_1">
+    	<input type="submit" name="submit">
+    </form>
+</body>
+</html>
+<?php
+
+
+if(isset($_POST['submit'])) {
+	
+	$cijfer_1 = $_POST['cijfer_1'];
+
+	if ($cijfer_1 >= 16.5) 
+	{
+    	echo "Je mag beginnen met rijlessen!";
+	}
+	else {
+    	echo "Helaas, je mag nog niet beginnen met rijlessen";
+	}
+}
+
+?>
